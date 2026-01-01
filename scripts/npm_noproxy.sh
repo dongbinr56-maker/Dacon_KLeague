5d8cb5a5f2ee0aeb0a01ed4a6424302539d10ece
dongbinr56-maker <dongbinr56@gmail.com>
2026-01-01 16:34:26 +0900

    Polish demo UX and document proxy-limited flows

--- scripts/npm_noproxy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-unset HTTP_PROXY HTTPS_PROXY http_proxy https_proxy NO_PROXY no_proxy
-unset npm_config_proxy npm_config_https_proxy npm_config_http_proxy
-
-REGISTRY=${NPM_REGISTRY:-https://registry.npmjs.org/}
-
-exec npm --registry "${REGISTRY}" "$@"
